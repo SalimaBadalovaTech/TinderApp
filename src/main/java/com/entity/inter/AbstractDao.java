8fae0985a01ed9a3c59038a72caff45b43e48d72
ARIF SULEYMANOV <arif.suleymanov0@gmail.com>
2022-10-26 15:37:36 +0400

    config2

--- src/main/java/com/entity/inter/AbstractDao.java
+++ src/main/java/com/entity/inter/AbstractDao.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 public class AbstractDao {
     public Connection connect() throws Exception {
         Class.forName("org.postgresql.Driver");
-//        String url = "jdbc:postgresql://ec2-34-241-90-235.eu-west-1.compute.amazonaws.com:5432/ddu83p2so5imac?sslmode=require";
         String url = "postgres://fnerjqwfyojtcl:be7c163971b7a147673a5e14a6c2e842ab706e50e1e209eb9cb3662d41716d80@ec2-3-220-207-90.compute-1.amazonaws.com:5432/dgoetfuaviiqk";
         String username = "fnerjqwfyojtcl";
         String password = "be7c163971b7a147673a5e14a6c2e842ab706e50e1e209eb9cb3662d41716d80";
